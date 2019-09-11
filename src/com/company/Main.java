6d46837de16effbb57e78af7a346dd10d05240e8
jnfrle <jennifer.lee1@montgomerycollege.edu>
2019-09-11 16:14:38 -0400

    Taste of OOP

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Person p = new Person();
+        p.setName("Jen");
+        p.setAge(100);
+
+        Person p2 = new Person("Betty", 99);
+    }
+}
